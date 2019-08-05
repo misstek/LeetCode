a3d5f9d73b58253fff3f61fc1eb0ff52571f245a
Mingzhen WANG <ming123zhen456@gmail.com>
2019-08-06 00:11:42 +1000

    Update 003 Longest Substring Without Repeating Characters.java

--- Java/003 Longest Substring Without Repeating Characters.java
+++ Java/003 Longest Substring Without Repeating Characters.java
@@ -1,37 +1,38 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         int sum = 0;
-	int A[] = new int[127];
-	int i;
+		int A[] = new int[127];
+		int i;
         
-	for(i=0 ; i<s.length() ; i++) {
-		for(int j = 0; j < 127 ;j++) {
-			A[j] = 0;
-		}
-		for(int j = i;j < s.length(); j++) {
-			int index = s.charAt(j);
-			if(A[index] != 0) {
-				int tempsum = 0;
-				for(int m = 0;m < 127;m++) {
-					tempsum += A[m];
-				}
-				if(tempsum > sum) {
-					sum = tempsum;
-				}
-				break;
+		for(i=0 ; i<s.length() ; i++) {
+			for(int j = 0; j < 127 ;j++) {
+				A[j] = 0;
 			}
-			else {
-				A[index] += 1;
+			
+			for(int j = i;j < s.length(); j++) {
+				int index = s.charAt(j);
+				if(A[index] != 0) {
+					int tempsum = 0;
+					for(int m = 0;m < 127;m++) {
+						tempsum += A[m];
+					}
+					if(tempsum > sum) {
+						sum = tempsum;
+					}
+					break;
+				}
+				else {
+					A[index] += 1;
+				}
 			}
-		}
-		int tempsum = 0;
-		for(int m = 0;m < 127;m++) {
+            int tempsum = 0;
+		    for(int m = 0;m < 127;m++) {
 		    	tempsum += A[m];
+		    }
+		    if(tempsum > sum) {
+		    	sum = tempsum;
+		    }
 		}
-		if(tempsum > sum) {
-			sum = tempsum;
-		}
-	}
-	    return sum;
+		return sum;
     }
 }
